4b7163a65316c44d151cb7df4b60262ed36f614e
rghj21 <92824386+rghj21@users.noreply.github.com>
2022-05-03 22:26:38 -0700

    Update DE1_SoC.sv

--- Lab 4/DE1_SoC.sv
+++ Lab 4/DE1_SoC.sv
@@ -12,9 +12,12 @@ module DE1_SoC(HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, SW, KEY, LEDR, CLOCK_50);
 	logic [3:0] out;
 	logic done, ready;
 	
-	bitCountingControl ctrl(.in(SW[7:0]), .start, .reset, .clk(CLOCK_50), .ready);
-	bitCountingDatapath path(.in(), .result(out), .done(done));
-	
-	task1HexDisplay hex (.result(out), .HEX0, .HEX1, .HEX2, .HEX3, .HEX4, .HEX5);
-	assign LEDR[9] = done;
+//	bitCountingControl ctrl(.in(SW[7:0]), .start, .reset, .clk(CLOCK_50), .ready);
+//	bitCountingDatapath path(.in(), .result(out), .done(done));
+//	
+//	task1HexDisplay hex (.result(out), .HEX0, .HEX1, .HEX2, .HEX3, .HEX4, .HEX5);
+//	assign LEDR[9] = done;
+	RAM32X8 (.address(addr), .clock(CLOCK_50), .data(0), .wren(0), .q(ramOut));
+	binarySearchDatapath(.ramOut, .clk(CLOCK_50), .currData, .addr, .ready, .increment, .decrement);
+	binarySearchControl(.A(SW[7:0]), .currData, .start, .reset, .clk(CLOCK_50), .dataFound, .correct, .ready, .increment, .decrement);
 endmodule
