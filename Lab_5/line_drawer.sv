b52873353c7e19febb574e806ba804dee9b14293
raccoondog11 <102640611+raccoondog11@users.noreply.github.com>
2022-05-07 16:14:30 -0700

    Line drawer

--- Lab_5/line_drawer.sv
+++ Lab_5/line_drawer.sv
@@ -21,11 +21,69 @@ module line_drawer(clk, reset, x0, y0, x1, y1, x, y, done);
 	output logic done;
 	output logic [10:0]	x, y;
 	
+	
+	
 	/* You'll need to create some registers to keep track of things
 	 * such as error and direction.
 	 */
 	logic signed [11:0] error;  // example - feel free to change/delete
+	logic [10:0] deltaX, deltaY, absX, absY, xFirst, xSecond, yFirst, ySecond, xStart, xEnd, yStart, yEnd, tempY ,outX ,outY;
+	logic isSteep, yStep;
 	
+	assign absX = (x0 > x1) ? (x0 - x1) : (x1 - x0);
+	assign absY = (y0 > y1) ? (y0 - y1) : (y1 - y0);
+	assign isSteep = (absY > absX) ? 1'b1 : 1'b0;
+	
+	always_comb begin
+		xFirst = isSteep ? y0 : x0;
+		xSecond = isSteep ? y1 : x1;
+		yFirst = isSteep ? x0 : y0;
+		ySecond = isSteep ? x1 : y1;
+		
+		xStart = (xFirst > xSecond) ? xSecond : xFirst;
+		xEnd = (xFirst > xSecond) ? xFirst : xSecond;
+		yStart = (xFirst > xSecond) ? ySecond : yFirst;
+		yEnd = (xFirst > xSecond) ? yFirst : ySecond;
+	end
+	
+	// int deltax = x1-x0 
+	assign deltaX = xEnd - xStart;
+   // int deltay = abs(y1-y0) 
+	assign deltaY = (yStart > yEnd) ? yStart - yEnd : yEnd - yStart;
+	// int error = -(deltax/2) 
+	assign error = -(deltaX/2);
+   // int y = y0 
+	assign tempY = yStart;
+	// if y0 < y1 then y_step = 1 else y_step = -1
+	assign yStep = (yStart < yEnd) ? 1'b1 : 1'b0;
+	
+	always_comb begin
+		if (xStart < xEnd) begin
+			if (isSteep) begin
+				outX = yStart;
+				outY = xStart;
+			end else begin
+				outX = xStart;
+				outY = yStart;
+			end
+			error = error + deltaY;
+			if (error >= 0) begin
+				if (yStep)
+					outY = outY + 1'b1;
+				else
+					outY = outY - 1'b1;
+				error = error - deltaX;
+			end
+			xStart = xStart + 1'b1;
+		end
+	end
+	
+	always_comb begin
+		x = outX;
+		y = outY;
+	end
+		
+
 	always_ff @(posedge clk) begin
 		// YOUR CODE HERE
 	end  // always_ff
